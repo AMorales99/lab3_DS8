790e1527febf0ba1a8e694539ec77fd0a3971a6b
Aurelio Morales <aurelio.morales@utp.ac.pa>
2021-11-14 16:14:23 -0500

    Editar Pantallas
    Se editaron las pantallas de movimientos y deposito/retiro

--- Laboratorio #3/Laboratorio3/Laboratorio3/FormDepoReti.Designer.vb
+++ Laboratorio #3/Laboratorio3/Laboratorio3/FormDepoReti.Designer.vb
@@ -23,6 +23,11 @@ Partial Class FormDepoReti
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.cbCliente = New System.Windows.Forms.ComboBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnDatos = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Label1
@@ -35,12 +40,65 @@ Partial Class FormDepoReti
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "DEPÓSITO / RETIRO"
         '
+        'cbCliente
+        '
+        Me.cbCliente.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbCliente.FormattingEnabled = True
+        Me.cbCliente.Location = New System.Drawing.Point(182, 102)
+        Me.cbCliente.Name = "cbCliente"
+        Me.cbCliente.Size = New System.Drawing.Size(276, 28)
+        Me.cbCliente.TabIndex = 13
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(106, 105)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 20)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Cliente:"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(182, 136)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(276, 28)
+        Me.ComboBox1.TabIndex = 15
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(104, 139)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 20)
+        Me.Label2.TabIndex = 14
+        Me.Label2.Text = "Cuenta:"
+        '
+        'btnDatos
+        '
+        Me.btnDatos.BackgroundImage = Global.Laboratorio3.My.Resources.Resources.lupa
+        Me.btnDatos.Location = New System.Drawing.Point(477, 121)
+        Me.btnDatos.Name = "btnDatos"
+        Me.btnDatos.Size = New System.Drawing.Size(75, 23)
+        Me.btnDatos.TabIndex = 16
+        Me.btnDatos.Text = "Button1"
+        Me.btnDatos.UseVisualStyleBackColor = True
+        '
         'FormDepoReti
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.LightBlue
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnDatos)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cbCliente)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label1)
         Me.Name = "FormDepoReti"
         Me.Text = "FormDepoReti"
@@ -50,4 +108,9 @@ Partial Class FormDepoReti
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents cbCliente As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnDatos As Button
 End Class
--- Laboratorio #3/Laboratorio3/Laboratorio3/FormMovimientos.Designer.vb
+++ Laboratorio #3/Laboratorio3/Laboratorio3/FormMovimientos.Designer.vb
@@ -23,6 +23,12 @@ Partial Class FormMovimientos
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cbCliente = New System.Windows.Forms.ComboBox()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.dtgMovimientos = New System.Windows.Forms.DataGridView()
+        CType(Me.dtgMovimientos, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -35,19 +41,80 @@ Partial Class FormMovimientos
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "MOVIMIENTOS DEL CLIENTE"
         '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(99, 132)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 20)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Cliente:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(99, 188)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(113, 20)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Movimientos:"
+        '
+        'cbCliente
+        '
+        Me.cbCliente.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cbCliente.FormattingEnabled = True
+        Me.cbCliente.Location = New System.Drawing.Point(175, 129)
+        Me.cbCliente.Name = "cbCliente"
+        Me.cbCliente.Size = New System.Drawing.Size(276, 28)
+        Me.cbCliente.TabIndex = 11
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBuscar.Location = New System.Drawing.Point(470, 127)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(110, 31)
+        Me.btnBuscar.TabIndex = 12
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'dtgMovimientos
+        '
+        Me.dtgMovimientos.BackgroundColor = System.Drawing.Color.White
+        Me.dtgMovimientos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dtgMovimientos.Location = New System.Drawing.Point(103, 211)
+        Me.dtgMovimientos.Name = "dtgMovimientos"
+        Me.dtgMovimientos.ReadOnly = True
+        Me.dtgMovimientos.RowHeadersVisible = False
+        Me.dtgMovimientos.Size = New System.Drawing.Size(607, 363)
+        Me.dtgMovimientos.TabIndex = 13
+        '
         'FormMovimientos
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.LightBlue
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(787, 586)
+        Me.Controls.Add(Me.dtgMovimientos)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.Controls.Add(Me.cbCliente)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label1)
         Me.Name = "FormMovimientos"
         Me.Text = "FormMovimientos"
+        CType(Me.dtgMovimientos, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents cbCliente As ComboBox
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents dtgMovimientos As DataGridView
 End Class
