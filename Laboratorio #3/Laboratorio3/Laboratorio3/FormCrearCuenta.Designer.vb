4fcfbb47770d2360eb2782ce22b5428d9fa760c9
PedroArias1800 <86125486+PedroArias1800@users.noreply.github.com>
2021-11-14 17:55:51 -0500

    Arreglo en la base de datos y algunos datos dentro

--- Laboratorio #3/Laboratorio3/Laboratorio3/FormCrearCuenta.Designer.vb
+++ Laboratorio #3/Laboratorio3/Laboratorio3/FormCrearCuenta.Designer.vb
@@ -26,20 +26,18 @@ Partial Class FormCrearCuenta
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.txtSaldo = New System.Windows.Forms.TextBox()
+        Me.cboTipoCuenta = New System.Windows.Forms.ComboBox()
         Me.btnCrear = New System.Windows.Forms.Button()
         Me.btnBorrar = New System.Windows.Forms.Button()
         Me.txtNombreCompleto = New System.Windows.Forms.TextBox()
-        Me.Label5 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(16, 36)
+        Me.Label1.Location = New System.Drawing.Point(13, 18)
         Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(301, 39)
@@ -49,103 +47,86 @@ Partial Class FormCrearCuenta
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(180, 273)
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(167, 245)
         Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(137, 25)
+        Me.Label2.Size = New System.Drawing.Size(164, 29)
         Me.Label2.TabIndex = 2
         Me.Label2.Text = "Saldo Inicial:"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(255, 217)
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(257, 188)
         Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(62, 25)
+        Me.Label3.Size = New System.Drawing.Size(74, 29)
         Me.Label3.TabIndex = 3
         Me.Label3.Text = "Tipo:"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(237, 160)
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(228, 129)
         Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(87, 25)
+        Me.Label4.Size = New System.Drawing.Size(103, 29)
         Me.Label4.TabIndex = 4
         Me.Label4.Text = "Cliente:"
         '
-        'TextBox1
+        'txtSaldo
         '
-        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(339, 273)
-        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(311, 30)
-        Me.TextBox1.TabIndex = 8
+        Me.txtSaldo.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSaldo.Location = New System.Drawing.Point(339, 242)
+        Me.txtSaldo.Margin = New System.Windows.Forms.Padding(4)
+        Me.txtSaldo.Name = "txtSaldo"
+        Me.txtSaldo.Size = New System.Drawing.Size(396, 34)
+        Me.txtSaldo.TabIndex = 8
+        Me.txtSaldo.Text = "0.00"
         '
-        'ComboBox1
+        'cboTipoCuenta
         '
-        Me.ComboBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(339, 156)
-        Me.ComboBox1.Margin = New System.Windows.Forms.Padding(4)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(367, 33)
-        Me.ComboBox1.TabIndex = 9
-        '
-        'ComboBox2
-        '
-        Me.ComboBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ComboBox2.FormattingEnabled = True
-        Me.ComboBox2.Location = New System.Drawing.Point(339, 217)
-        Me.ComboBox2.Margin = New System.Windows.Forms.Padding(4)
-        Me.ComboBox2.Name = "ComboBox2"
-        Me.ComboBox2.Size = New System.Drawing.Size(396, 33)
-        Me.ComboBox2.TabIndex = 10
+        Me.cboTipoCuenta.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cboTipoCuenta.FormattingEnabled = True
+        Me.cboTipoCuenta.Location = New System.Drawing.Point(339, 185)
+        Me.cboTipoCuenta.Margin = New System.Windows.Forms.Padding(4)
+        Me.cboTipoCuenta.Name = "cboTipoCuenta"
+        Me.cboTipoCuenta.Size = New System.Drawing.Size(396, 37)
+        Me.cboTipoCuenta.TabIndex = 10
         '
         'btnCrear
         '
-        Me.btnCrear.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnCrear.Location = New System.Drawing.Point(468, 361)
+        Me.btnCrear.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCrear.Location = New System.Drawing.Point(490, 341)
         Me.btnCrear.Margin = New System.Windows.Forms.Padding(4)
         Me.btnCrear.Name = "btnCrear"
-        Me.btnCrear.Size = New System.Drawing.Size(147, 38)
+        Me.btnCrear.Size = New System.Drawing.Size(204, 51)
         Me.btnCrear.TabIndex = 11
         Me.btnCrear.Text = "Crear"
         Me.btnCrear.UseVisualStyleBackColor = True
         '
         'btnBorrar
         '
-        Me.btnBorrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.btnBorrar.Location = New System.Drawing.Point(309, 361)
+        Me.btnBorrar.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBorrar.Location = New System.Drawing.Point(262, 341)
         Me.btnBorrar.Margin = New System.Windows.Forms.Padding(4)
         Me.btnBorrar.Name = "btnBorrar"
-        Me.btnBorrar.Size = New System.Drawing.Size(147, 38)
+        Me.btnBorrar.Size = New System.Drawing.Size(204, 51)
         Me.btnBorrar.TabIndex = 12
         Me.btnBorrar.Text = "Borrar"
         Me.btnBorrar.UseVisualStyleBackColor = True
         '
         'txtNombreCompleto
         '
-        Me.txtNombreCompleto.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.txtNombreCompleto.Location = New System.Drawing.Point(339, 103)
+        Me.txtNombreCompleto.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtNombreCompleto.Location = New System.Drawing.Point(339, 126)
         Me.txtNombreCompleto.Name = "txtNombreCompleto"
-        Me.txtNombreCompleto.Size = New System.Drawing.Size(367, 34)
+        Me.txtNombreCompleto.ReadOnly = True
+        Me.txtNombreCompleto.Size = New System.Drawing.Size(396, 34)
         Me.txtNombreCompleto.TabIndex = 13
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(468, 13)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(51, 17)
-        Me.Label5.TabIndex = 13
-        Me.Label5.Text = "Label5"
         '
         'FormCrearCuenta
         '
@@ -154,12 +135,10 @@ Partial Class FormCrearCuenta
         Me.BackColor = System.Drawing.Color.LightBlue
         Me.ClientSize = New System.Drawing.Size(948, 450)
         Me.Controls.Add(Me.txtNombreCompleto)
-        Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.btnBorrar)
         Me.Controls.Add(Me.btnCrear)
-        Me.Controls.Add(Me.ComboBox2)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.cboTipoCuenta)
+        Me.Controls.Add(Me.txtSaldo)
         Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
@@ -176,11 +155,9 @@ Partial Class FormCrearCuenta
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents txtSaldo As TextBox
+    Friend WithEvents cboTipoCuenta As ComboBox
     Friend WithEvents btnCrear As Button
     Friend WithEvents btnBorrar As Button
     Friend WithEvents txtNombreCompleto As TextBox
-    Friend WithEvents Label5 As Label
 End Class
--- Laboratorio #3/Laboratorio3/Laboratorio3/FormCrearCuenta.vb
+++ Laboratorio #3/Laboratorio3/Laboratorio3/FormCrearCuenta.vb
@@ -2,14 +2,40 @@
 Imports System.Data.SqlClient
 Imports System.Net
 Public Class FormCrearCuenta
-    Private Sub btnCrear_Click(sender As Object, e As EventArgs) Handles btnCrear.Click
-
-
-
-
+    Private Sub FormCrearCuenta_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cboTipoCuenta.Items.Add("Cuenta De Ahorros")
+        cboTipoCuenta.Items.Add("Cuenta Corriente")
+        cboTipoCuenta.SelectedIndex = 0
     End Sub
 
     Private Sub btnBorrar_Click(sender As Object, e As EventArgs) Handles btnBorrar.Click
-
+        txtSaldo.Text = 0.00
     End Sub
+    Private Sub btnCrear_Click(sender As Object, e As EventArgs) Handles btnCrear.Click
+        Dim query As String
+        Dim comando As SqlCommand
+        Dim cboTipo = 0
+        If cboTipoCuenta.Text = "Cuenta De Ahorros" Then
+            cboTipo = 1
+        End If
+        query = "Insert into tbl_datoscliente (id_cliente, nombre, apellido, telefono_movil, direccion, fecha_creacion)Values ("
+        query &= "" & 4 & ""
+        query &= ",'" & cboTipo "'"
+        query &= ",'" & txtApellido1.Text & "'"
+        query &= ",'" & txtTelefono1.Text & "'"
+        query &= ",'" & rtxtDireccion1.Text & "'"
+        query &= ", GETDATE()" & ")"
+        Try
+
+            comando = New SqlCommand(query, mYConn)
+            mYConn.Open()
+            comando.ExecuteNonQuery()
+            MessageBox.Show("Los Datos se insertaron. ")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        Finally
+            If mYConn.State <> ConnectionState.Closed Then mYConn.Close()
+        End Try
+    End Sub
+
 End Class
